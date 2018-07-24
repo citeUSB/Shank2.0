006b4568747c3b349ec30d7de6e3fd0aca0f0b18
Victor Garcia <victorygarciac@gmail.com>
2018-07-23 23:35:49 -0400

    Serial Funcionando
    Envio de datos entre shank y el DUE

---
+++ Programacion/Serial/EnvioSerial/EnvioSerial.ino
@@ -0,0 +1,27 @@
+/*
+  Multple serial test
+ 
+  Receives from the main serial port, sends to the others. 
+  Receives from serial port 1, sends to the main serial (Serial 0).
+ 
+  The circuit: 
+  * Any serial device attached to Serial port 1
+  * Serial monitor open on Serial port 0:
+ 
+  created 30 Dec. 2008
+  by Tom Igoe
+ 
+  This example code is in the public domain.
+ 
+*/
+
+
+void setup() {
+  // initialize both serial ports:
+  Serial.begin(115200);
+}
+
+void loop() {
+  Serial.write('c');
+  delay(500);
+}
