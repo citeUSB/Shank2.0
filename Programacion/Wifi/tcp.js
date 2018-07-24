7622e83aea010770a73d2bab8b30f64fd4cee27a
Gabriel <gabrielnoya95@gmail.com>
2018-07-24 11:30:31 -0400

    Consola para comunicacion con ESP

--- Programacion/Wifi/tcp.js
+++ Programacion/Wifi/tcp.js
@@ -7,33 +7,37 @@ const rl = readline.createInterface({
 });
 
 let client = new net.Socket();
-client.connect(9876, '192.168.0.1', function() {
-	console.log('Connected');
+client.connect(9876, '192.168.0.1', function () {
+  //console.log('Connected');
 });
 
-client.on('data', function(data) {
-  medicion = (data[0])|(data[1]<<8)|(data[2]<<16)||(data[3]<<24)
-  console.log(dictionary[state] + medicion)
+client.on('data', function (data) {
+  medicion = (data[0]) | (data[1] << 8) | (data[2] << 16) || (data[3] << 24)
+  console.log(dictionary[parseInt(state)] + medicion)
 });
 
-let state = 1;
+let state = '1';
 let dictionary = {
   1: "Led 1: ",
-  3: "Led 3: ", 
+  3: "Led 3: ",
   4: "Led 4: ",
   6: "Led 6: ",
   7: "Ángulo: ",
   8: "Laberinto: "
 };
 
-// function loop(){
-//   rl.question('> ', (answer) => {
-//     console.log(answer);
-//     console.log('')
-//     state = parseInt(answer);
-//     client.write(answer);
-//     loop();
-//   });
-// }
+function loop() {
+  rl.question('> ', (answer) => {
+    //console.log(answer);
+    state = answer;
+    loop();
+  });
+}
 
-// loop();
+loop();
+
+setInterval(write, 100);
+
+function write() {
+  client.write(state);
+}
