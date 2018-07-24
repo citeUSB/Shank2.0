034b934e429c647ad34f300f4326a7d4b746f31c
Victor Garcia <victorygarciac@gmail.com>
2018-07-23 23:44:41 -0400

    EnviandoBuffer
    Envio de datos con un buffer

--- Programacion/Serial/EnvioSerial/EnvioSerial.ino
+++ Programacion/Serial/EnvioSerial/EnvioSerial.ino
@@ -1,27 +1,20 @@
-/*
-  Multple serial test
- 
-  Receives from the main serial port, sends to the others. 
-  Receives from serial port 1, sends to the main serial (Serial 0).
- 
-  The circuit: 
-  * Any serial device attached to Serial port 1
-  * Serial monitor open on Serial port 0:
- 
-  created 30 Dec. 2008
-  by Tom Igoe
- 
-  This example code is in the public domain.
- 
-*/
-
+uint32_t medicion;
+char buffer[5];
 
 void setup() {
+  medicion = 155;
   // initialize both serial ports:
   Serial.begin(115200);
+  buffer[0] = medicion;
+  buffer[1] = medicion>>8;
+  buffer[2] = medicion>>16;
+  buffer[3] = medicion>>24;
+  buffer[4] = 0xFF;
 }
 
 void loop() {
-  Serial.write('c');
+  for (int i=0;i<5;i++){
+    Serial.write(buffer[i]);
+  }
   delay(500);
 }
--- Programacion/Serial/LecturaSerial/LecturaSerial.ino
+++ Programacion/Serial/LecturaSerial/LecturaSerial.ino
@@ -1,24 +1,5 @@
-/*
-  Multiple Serial test
-
-  Receives from the main serial port, sends to the others.
-  Receives from serial port 1, sends to the main serial (Serial 0).
-
-  This example works only with boards with more than one serial like Arduino Mega, Due, Zero etc.
-
-  The circuit:
-  - any serial device attached to Serial port 1
-  - Serial Monitor open on Serial port 0
-
-  created 30 Dec 2008
-  modified 20 May 2012
-  by Tom Igoe & Jed Roach
-  modified 27 Nov 2015
-  by Arturo Guadalupi
-
-  This example code is in the public domain.
-*/
-
+uint32_t medicion;
+char buffer[4];
 
 void setup() {
   // initialize both serial ports:
@@ -28,8 +9,11 @@ void setup() {
 
 void loop() {
   // read from port 1, send to port 0:
-  if (Serial1.available() > 0) {
+  /*if (Serial1.available() > 0) {
    char inByte = Serial1.read();
    Serial.println(inByte); 
-  }
+  }*/
+  Serial1.readBytesUntil(0xFF,buffer,4);
+  medicion = ((uint32_t)buffer[0])|((uint32_t)buffer[1]<<8)|((uint32_t)buffer[2]<<16)|((uint32_t)buffer[3]<<24);
+  Serial.println(medicion); 
 }
