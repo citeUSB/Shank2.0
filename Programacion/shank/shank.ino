5d6a46c4d8daaf8cd1db508329948cfb7071bb67
Andres Suarez <suarez2194@gmail.com>
2018-11-02 10:51:49 -0400

    Regreso a la vida

--- Programacion/shank/shank.ino
+++ Programacion/shank/shank.ino
@@ -54,6 +54,11 @@ void loop() {
   }
   enviarSerial(selectorDato(selector - 48),6);
   enviarSerial(selectorDato(selector - 48),1);
+  digitalWrite(LED1,LOW);
+  digitalWrite(LED2,LOW);
+  delay(300);
+  digitalWrite(LED1,HIGH);
+  digitalWrite(LED2,HIGH);
   delay(300);
 }
 
